c2a76349848ec408baf51ce1eca324d81f622624
evansteelepdx <evan.steele@evansteele.net>
2023-04-01 00:18:59 -0700

    added express backend with routing

---
+++ src/app/App.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App(): JSX.Element {
+  const [text, setText] = useState("");
+  const [apiKey, setApiKey] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+  function handleApiKeychange(event: React.ChangeEvent<HTMLInputElement>) {
+    setApiKey(event.target.value);
+  }
+
+  function handleTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  function handleApiKeySubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(`API key saved! ${apiKey}`)
+  }
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setLoading(true);
+    console.log(`Sending to server! ${text}`)
+    setLoading(false);
+  }
+
+  return (
+    <Router>
+    <div className="App">
+    <div className="wrapper">
+    <div className="one">
+    API Key:
+    <form onSubmit={handleApiKeySubmit}>
+    <input type="text" value={apiKey} onChange={handleApiKeychange}/>
+    <button type="submit">Submit</button>
+    </form>
+    Input text:
+    <form onSubmit={handleSubmit}>
+    <input type="text" value={text} onChange={handleTextChange} />
+    <button type="submit">Submit</button>
+    </form>
+    </div>
+    <div className="two">Response from API goes here!</div>
+    <div className="three">
+    API Response: {loading ? "Loading..." : "Submit"}
+    </div>
+    </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
