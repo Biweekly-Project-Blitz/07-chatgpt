c2bca7451b843e97b3012a0d9d5ac427b19b4381
evansteelepdx <evan.steele@evansteele.net>
2023-04-02 14:50:21 -0700

    added model call to test api key

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -7,6 +8,8 @@ function App(): JSX.Element {
   const [text, setText] = useState("");
   const [apiKey, setApiKey] = useState("");
   const [loading, setLoading] = useState(false);
+  const [auth, setAuth] = useState(false);
+  const [apiResponses, setResponses] = useState([]);
 
 
   function handleApiKeychange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -17,16 +20,43 @@ function App(): JSX.Element {
     setText(event.target.value);
   }
 
-  function handleApiKeySubmit(event: React.FormEvent<HTMLFormElement>) {
+  async function handleApiKeySubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log(`API key saved! ${apiKey}`)
+    // TODO: test key validity using /models/ endpoint
+    try {
+      const response = await axios.post('/api/input', {
+        "key":apiKey
+      });
+      console.log(response);
+    } catch (error){
+      console.error(error);
+    } finally {
+      console.log('API test call complete')
+    }
+    setAuth(true); 
   }
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setLoading(true);
-    console.log(`Sending to server! ${text}`)
+    console.log(`Sending to server! ${text} using API key ${apiKey}`)
     setLoading(false);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post('/api/my-endpoint', {
+        // Request body data goes here
+      });
+      // Handle response data here
+    } catch (error) {
+      // Handle error here
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -40,10 +70,16 @@ function App(): JSX.Element {
     <button type="submit">Submit</button>
     </form>
     Input text:
-    <form onSubmit={handleSubmit}>
-    <input type="text" value={text} onChange={handleTextChange} />
-    <button type="submit">Submit</button>
-    </form>
+    <div>
+    {auth ? (
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={handleTextChange} />
+      <button type="submit">Submit</button>
+      </form>
+    ) : (
+      <p>Please provide a valid OpenAPI key</p>
+    )}
+    </div>
     </div>
     <div className="two">Response from API goes here!</div>
     <div className="three">
--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import axios from 'axios';
 
 const router = express.Router();
 
 router.get('/hello', async (_req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
+
+router.post('/input', async(_req,res) => {
+  axios.get('https://api.openai.com/v1/models',{
+    headers:{
+      Authorization: `Bearer ${_req.body.key}`
+    }
+  }
+  )
+    .then(response => {
+      console.log(response.data);
+      res.status(200).json({"result":response.data});
+    })
+    .catch(error =>{
+      console.log(error.response.data)
+      res.status(401).json({"result":error.response.data})
+    })
+});
+
 
 export default router;
